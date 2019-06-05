6c417032f89ecccd0cca71d6854be99e2c76e4eb
AraceliRdz <51061115+AraceliRdz@users.noreply.github.com>
2019-06-05 06:23:59 -0500

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,17 @@
+<?php
+ 
+$ruta=$_GET['C:\Users\Pinol\odrive\Google Cloud Storage'];
+ 
+$directorio = opendir($ruta); //ruta actual
+while ($archivo = readdir($directorio)) //obtenemos un archivo y luego otro sucesivamente
+{
+    if (is_dir($archivo))//verificamos si es o no un directorio
+    {
+        echo "[".$archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes
+    }
+    else
+    {
+        echo $archivo . "<br />";
+    }
+}
+?>
